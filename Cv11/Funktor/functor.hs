526a3a94d935091caceda7bf6028dd3f91b72547
borovan <borovan@microstep-mis.com>
2017-05-15 22:43:40 +0200

    jedenaste cvicenie

---
+++ Cv11/Funktor/functor.hs
@@ -0,0 +1,29 @@
+{-
+instance Functor Maybe where
+  -- fmap :: (a -> b) -> Maybe a -> Maybe b
+  fmap _ Nothing = Nothing
+  fmap g (Just x) = Just (g x)
+-}
+
+-- vyhodnotte
+e1 = fmap (+1) Nothing
+e2 = fmap (*2) (Just 3)
+e3 = fmap not (Just False)
+
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving(Show)
+
+instance Functor Tree where
+  fmap g (Leaf x) = Leaf (g x)
+  fmap g (Node l r) = Node (fmap g l) (fmap g r)
+
+-- vyhodnotte  
+e4 = fmap length (Leaf "abc")
+e5 = fmap even (Node (Leaf 1) (Leaf 2))
+
+inc :: Functor f => f Int -> f Int
+inc = fmap (+1)
+
+e6 = inc (Just 1)
+e7 = inc [1,2,3,4,5]
+e8 = inc (Node (Leaf 1) (Leaf 2))
+-- Functors abstract the idea of mapping a function over each element of a structure
